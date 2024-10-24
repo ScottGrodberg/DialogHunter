9fc55702358864e1ca1983f066caeb696d9afddf
Scott Grodberg <scott@trwebco.com>
2024-10-24 16:02:17 -0400

    More wiring, pointer event stub for Interchange.

--- src/Interchange.ts
+++ src/Interchange.ts
@@ -1,7 +1,23 @@
+import { Choice } from "./Choice.js";
+
 export class Interchange {
+    element: HTMLElement;
+    choices: Array<Choice>;
 
-    dep() {
-        console.log(`dep2`);
+    constructor(public svgns: any) {
+        this.choices = new Array();
 
+        this.element = this.rectangle(80, 40);
+        this.element.onpointerdown = () => {
+            console.log(`pointer down`);
+        };
+    }
+
+    rectangle(width: number, height: number): HTMLElement {
+        const rect = document.createElementNS(this.svgns, "rect");
+        rect.setAttribute("width", width.toString());
+        rect.setAttribute("height", height.toString());
+        rect.setAttribute("style", "fill:red;stroke:black;stroke-width:2;margin:10px;padding:10;");
+        return rect;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Interchange } from "./Interchange.js";
 
+let svgns: any;
 
 window.addEventListener("DOMContentLoaded", () => {
     const main = new Main();
@@ -8,36 +9,23 @@ window.addEventListener("DOMContentLoaded", () => {
 
 export class Main {
 
-    svgns: any;
 
     test() {
-        const dep = new Interchange();
-        dep.dep();
 
         // Create the SVG namespace
-        this.svgns = "http://www.w3.org/2000/svg";
+        svgns = "http://www.w3.org/2000/svg";
 
         // Create an SVG element
-        const svg = document.createElementNS(this.svgns, "svg");
+        const svg = document.createElementNS(svgns, "svg");
         svg.setAttribute("width", "200");
         svg.setAttribute("height", "100");
         document.body.appendChild(svg);
 
 
-        const rect = this.rectangle(80, 40);
-        rect.onpointerdown = () => {
+        const interchange = new Interchange(svgns);
+        svg.appendChild(interchange.element);
 
-        };
-
-        svg.appendChild(rect);
     }
 
-    rectangle(width: number, height: number) {
-        const rect = document.createElementNS(this.svgns, "rect");
-        rect.setAttribute("width", width.toString());
-        rect.setAttribute("height", height.toString());
-        rect.setAttribute("style", "fill:red;stroke:black;stroke-width:2;margin:10px;padding:10;");
-        return rect;
-    }
 
 }
