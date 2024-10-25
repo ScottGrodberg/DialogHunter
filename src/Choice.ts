f737f91d8859bd3700f3535699bab08151fb9056
Scott Grodberg <scott@trwebco.com>
2024-10-25 17:23:17 -0400

    Introduce sockets.

--- src/Choice.ts
+++ src/Choice.ts
@@ -5,11 +5,28 @@ export class Choice {
         this.element = document.createElement("div");
         this.element.style.display = "flex";
         this.element.style.flexDirection = "row";
+        this.element.style.position = "relative";
+
+        const socket = document.createElement("div");
+        socket.style.borderRadius = "50%";
+        socket.style.backgroundColor = "white";
+        socket.style.width = "14px";
+        socket.style.height = "14px";
+        socket.style.position = "absolute";
+        socket.style.border = "1px solid black";
+        socket.style.top = "10px";
+
+        const socketLeft = socket.cloneNode() as HTMLElement;
+        socketLeft.style.left = "-18px";
+        const socketRight = socket.cloneNode() as HTMLElement;
+        socketRight.style.left = "calc(100% + 2px)";
 
         const key = document.createElement("input");
         key.style.width = "20%";
+
         const value = document.createElement("textarea");
         value.style.width = "60%"
+
         const x = document.createElement("button");
         x.innerHTML = "X";
         x.style.width = "20%"
@@ -17,8 +34,10 @@ export class Choice {
             (event.target as HTMLElement).parentElement!.remove();
         };
 
+        this.element.appendChild(socketLeft);
         this.element.appendChild(key);
         this.element.appendChild(value);
         this.element.appendChild(x);
+        this.element.appendChild(socketRight);
     }
 }
