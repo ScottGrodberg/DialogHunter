61defba202f4c7e65c61b408a30c53ac8c1d9dbe
Scott Grodberg <scott@trwebco.com>
2024-10-31 09:09:30 -0400

    Introduce text variable to node.

--- src/Node.ts
+++ src/Node.ts
@@ -2,6 +2,8 @@ import { ChoiceId, NodeId } from "./Data";
 
 export class Node {
     position?: { top: number, left: number };
+    text?: string; // statement or question or description
+
     choices: Array<ChoiceId>
 
     constructor(public nodeId: NodeId) {
