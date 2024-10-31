95feefeb630cbb71463c1b3ddbbec3abafdfa10f
Scott Grodberg <scott@trwebco.com>
2024-10-31 13:46:48 -0400

    Tweak the arrow.

--- src/CurrentNode.ts
+++ src/CurrentNode.ts
@@ -10,14 +10,14 @@ export class CurrentNode {
 
         const element = document.getElementById(`node-${nodeId}`)!;
         this.arrow!.setAttribute("x", element.style.left);
+        this.arrow!.setAttribute("y", parseInt(element.style.top) - 30 + "px");
     }
-
 
     makeCurrentArrow() {
         const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg.setAttribute("x", "-200px"); // start offscreen to the left 
         const arrow = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        arrow.setAttribute("d", "M 70 10 L 130 30 L 10 30 Z");
+        arrow.setAttribute("d", "M 0 0 L 170 0 L 85 20 Z");
         arrow.setAttribute("fill", "black");
         svg.appendChild(arrow);
         this.arrow = svg;
